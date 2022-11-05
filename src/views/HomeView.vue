db1664c60c294cfb77dd3af8e27527b55538a957
Marko Obradovic <marko123obradovic@gmail.com>
2022-11-05 19:05:58 +0100

    updating design - changing filters (in progress)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,14 @@
 <div class="home-container">
   <span class="home-title">Find your next stay</span>
   <Search @search="redirect" :vertical="false"/>
-  <!-- <section class="w-100 mt-5">
+  <section class="w-100 mt-5">
     <div class="section-div">
       <div class="section-header">
         <span class="section-title">Delve into the gems of Serbia</span>
         <span class="section-desc">Here are some popular destinations to explore</span>
       </div>
-      <div class="d-flex f-wrap w-100">
-        <div class="card c-pointer" v-for="des in destinations" :key="des.des_id">
-          <a class="card-img"  v-if="des.images" href="#">
-            <img v-for="img in des.images" :key="img.file_id" :src="img.image_url" alt="">
-          </a>
-          <span>{{des.des_name}}</span>
-        </div>
-      </div>
     </div>
-  </section> -->
+  </section>
 </div>
 </template>
 
--- src/views/HotelsView.vue
+++ src/views/HotelsView.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="d-flex j-center w-100 mt-3">
+    <Search :vertical="false" @search="getHotels()"/>
+  </div>
   <div class="section-div d-flex j-center a-center f-wrap mt-2">
     <div class="tag-button d-flex a-center">
       <img src="../assets/icons8-tag-window-100.png" alt="">
@@ -29,48 +32,46 @@
       Popular
     </div>
   </div>
+  <div class="filter-sort-div">
+    <button class="filter-btn">Filters</button>
+    <button class="sort-btn">Sort by</button>
+  </div>
   <div class="section-div d-flex j-center mt-2" id="hotels-view">
-      <div class="slider-menu" id="slider">
-        <div class="filters">
-          <Search :vertical="true" @search="getHotels()"/>
-          <div class="filter-search-div">
-            <div class="filters-title">
-              <span class="title">Filters</span>
-            </div>
-            <div class="filters-content">
-              <div class="filter-inputs-div">
-                <label class="filter-label">Price:</label>
-                <div class="d-flex j-center w-100">
-                  <div class="d-flex f-col a-center">
-                    <label>From:</label>
-                    <input class="price-input" type="number" v-model="filters.start_price">
-                  </div>
-                  <div class="d-flex f-col a-center">
-                    <label>To:</label>
-                    <input class="price-input" type="number" v-model="filters.end_price"> 
-                  </div>
+      <div class="filters">
+        <div class="filter-search-div">
+          <div class="filters-title">
+            <span class="title">Filters</span>
+          </div>
+          <div class="filters-content">
+            <div class="filter-inputs-div">
+              <label class="filter-label">Price:</label>
+              <div class="d-flex j-center w-100">
+                <div class="d-flex f-col a-center">
+                  <label>From:</label>
+                  <input class="price-input" type="number" v-model="filters.start_price">
                 </div>
-                <div class="submit-btn">
-                  <button class="search-btn" @click="getHotels(filters.start_price, filters.end_price)"
-                  >Submit</button>
+                <div class="d-flex f-col a-center">
+                  <label>To:</label>
+                  <input class="price-input" type="number" v-model="filters.end_price"> 
                 </div>
-                <div class="border-b w-100">
-
-                </div>
-                <div class="mt-1">
-                  <label class="filter-label">Features</label>
-                </div>
-                <div class="features-div">
-                  <div class="features-item" v-for="fea in featuresDb" :key="fea.fea_id">
-                    <input type="checkbox" class="checkbox" :value="fea.fea_id" @change="getCheckedFeatures()">
-                    <label>{{fea.fea_name}}</label>
-                  </div>
+              </div>
+              <div class="submit-btn">
+                <button class="search-btn" @click="getHotels(filters.start_price, filters.end_price)"
+                >Submit</button>
+              </div>
+              <div class="border-b w-100"></div>
+              <div class="mt-1">
+                <label class="filter-label">Features</label>
+              </div>
+              <div class="features-div">
+                <div class="features-item" v-for="fea in featuresDb" :key="fea.fea_id">
+                  <input type="checkbox" class="checkbox" :value="fea.fea_id" @change="getCheckedFeatures()">
+                  <label>{{fea.fea_name}}</label>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <img @click="showSlider()" class="slider-btn" src="../assets/arrow.png" alt="" id="slider-btn">
       </div>
     <div class="hot-list-container">
       <HotelsList :hotels="hotels" @loaded='this.isLoadedList = true'/>
@@ -79,9 +80,6 @@
 </template>
 
 <script>
-const x = window.matchMedia("(min-width: 850px)")
-const xMax = window.matchMedia("(max-width: 850px)")
-
 import service from '../services/API'
 import HotelsList from '../components/HotelsList.vue'
 import Search from '../components/Search-Component.vue'
@@ -96,31 +94,6 @@ export default{
   mounted(){
     this.getHotels()
     this.getFeatures()
-
-    window.addEventListener('resize', () => {
-      if(window.innerWidth <= 850){
-        this.showVerSearch = false
-        console.log(this.showVerSearch);
-      }
-      else{
-        this.showVerSearch = true
-        console.log(this.showVerSearch);
-      }
-    })
-
-    x.addEventListener('change', () => {
-      let slider = document.getElementById('slider')
-      let btn = document.getElementById('slider-btn')
-      
-      if(x.matches && slider.style.transform !== "translateX(0rem)"){
-        slider.style.transform = "translateX(0rem)"
-        btn.style.transform = "rotateZ(0deg)"
-      }
-      else if(xMax.matches && slider.style.transform == "translateX(0rem)"){
-        slider.style.transform = "translateX(-17.2rem)"
-        btn.style.transform = "rotateZ(0deg)"
-      }
-    })
   },
   data(){
     return{
@@ -164,66 +137,22 @@ export default{
       console.log(this.filters.featuresArr);
       this.getHotels()
     },
-    showSlider(){
-      let btn = document.getElementById('slider-btn')
-      let el = document.getElementById('slider')
-      if(el.style.transform !== "translateX(0rem)"){
-        el.style.transform = "translateX(0rem)"
-        btn.style.transform = "rotateZ(180deg)"
-      }
-      else{
-        el.style.transform = "translateX(-17.2rem)"
-        btn.style.transform = "rotateZ(0deg)"
-      }
-    }
+    // showSlider(){
+    //   let btn = document.getElementById('slider-btn')
+    //   let el = document.getElementById('slider')
+    //   if(el.style.transform !== "translateX(0rem)"){
+    //     el.style.transform = "translateX(0rem)"
+    //     btn.style.transform = "rotateZ(180deg)"
+    //   }
+    //   else{
+    //     el.style.transform = "translateX(-17.2rem)"
+    //     btn.style.transform = "rotateZ(0deg)"
+    //   }
+    // }
   },
 }
 </script>
-<style>
-.tag-button{
-  padding: 0.4rem;
-  margin: 0.5rem;
-  cursor: pointer;
-  color: gray;
-  display: flex;
-  align-items: center;
-  transition: all 0.2s;
-  border: 2px solid lightgray;
-  border-radius: 10px;
-  background-color: white;
-}
-.tag-button img{
-  width: 1.5rem;
-  margin-right: 0.5rem;
-}
-.tag-button:hover{
-  background-color: rgb(110, 109, 175);
-  color: white;
-  border: 2px solid rgb(187, 187, 152);
-}
-.submit-btn{
-  margin-bottom: 0.5rem;
-  margin-top: 0.5rem;
-}
 
-.features-div{
-  margin-top: 1rem;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding-left: 1rem;
-  padding-bottom: 0.5rem;
-}
-.features-item{
-  display: flex;
-  align-items: center;
-  margin: 0.5rem;
-}
-.features-item input{
-  width: 1.3rem;
-  margin-right: 0.5rem;
-  cursor: pointer;
-  box-shadow: 0 0 3px 1px wheat;
-}
+<style>
+
 </style>
